b739938b2206fa0afdbe453552010d66c141acf9
Satiwan kumar <satiwankumar12@gmail.com>
2020-08-03 13:58:45 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      <h1>Hello world</h1>
+      <h1>Hello wali</h1>
       </header>
     </div>
   );
